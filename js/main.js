540d22c1ea21442beb4f7e151beec8e6835818a4
Nick Teaford <nickteaford@Nicks-iMac.local>
2021-11-23 14:37:32 -0800

    11/23 afternoon commit

--- js/main.js
+++ js/main.js
@@ -4,46 +4,70 @@ let playerTurn, currentSelection, currentSpace, moveOption1, moveOption2, moveOp
 
 /*----- constants -----*/
 const gameState = [];
-    for (let i = 1; i <= 64; i++) {
-    gameState.push("");
-    }
+const blkTurnLookup = {
+    tokenUsed: "black--token",
+}
 
-    const blkTurnLookup = {
-        tokenUsed: "black--token",
-    }
-
-    const redTurnLookup = {
-        tokenUsed: "red--token",
-    }
-
-    
-    
-    
-    /*----- cached element references -----*/
-    
-    
-    
-    /*----- event listeners -----*/
-    document.querySelector(".game--container").addEventListener('click', moveCalculator);
-    
-    
-    /*----- functions -----*/
-    
-    init();
-    
-    function init () {
-        playerTurn = blkTurnLookup;
-    currentSpace = 0;
+const redTurnLookup = {
+    tokenUsed: "red--token",
 }
 
 
+
+
+/*----- cached element references -----*/
+
+
+
+/*----- event listeners -----*/
+document.querySelector(".game--container").addEventListener('click', moveCalculator);
+document.querySelector("p").addEventListener('click', moveToken);
+
+
+/*----- functions -----*/
+
+init();
+
+function init () {
+    playerTurn = blkTurnLookup;
+    currentSpace = 0;
+
+    for (let i = 1; i <= 64; i++) {
+        gameState.push("");
+        for (let i = 1; i <= 7; i+=2) {
+            gameState[i] = "red--token";
+            }
+        for (let i = 8; i <= 14; i+=2) {
+            gameState[i] = "red--token";
+            }
+        for (let i = 17; i <= 23; i+=2) {
+            gameState[i] = "red--token";
+            }
+        for (let i = 62; i >= 56; i-=2) {
+            gameState[i] = "black--token";
+                }
+        for (let i = 55; i >= 49; i-=2) {
+            gameState[i] = "black--token";
+                }
+        for (let i = 46; i >= 40; i-=2) {
+            gameState[i] = "black--token";
+                }
+    }
+}
+    
+
 function moveCalculator(e) {
+    //reset checkerboard squares to grey
+    let x = document.querySelectorAll(".potentialMove");
+    console.log(x);
+    x.forEach(checkerbox => checkerbox.classList.remove("potentialMove"));
+    console.log(x);
     //set current token type
     tokenRankSelected = tokenTypeLookup(e);
     //confirm that this token is allowed to move
     tokenTeamSelected = turnCheck(e);
     //set currentSpace variable
-    currentSpace = parseInt(currentSpace = e.target.parentElement.getAttribute("data-cell-index"));
+    currentSpace = parseInt(currentSpace = e.target.parentElement.getAttribute("id"));
     
     
     const moveLookup = {
@@ -52,25 +76,25 @@ function moveCalculator(e) {
                 moveOption1: (currentSpace - 9),
                 moveOption2: (currentSpace - 7),
                 restrictedSquares: {
-                    9: { moveOption1: null, 
+                    8: { moveOption1: null, 
                         moveOption2: (currentSpace - 7), 
                     },
-                    24: { moveOption1:(currentSpace - 9),
+                    23: { moveOption1:(currentSpace - 9),
                         moveOption2: null,
                     },
-                    25: { moveOption1: null, 
+                    24: { moveOption1: null, 
                         moveOption2: (currentSpace - 7), 
                     },
-                    40: { moveOption1:(currentSpace - 9),
+                    39: { moveOption1:(currentSpace - 9),
                         moveOption2: null,
                     },
-                    41: { moveOption1: null, 
+                    40: { moveOption1: null, 
                         moveOption2: (currentSpace - 7), 
                     },
-                    56: { moveOption1:(currentSpace - 9),
+                    55: { moveOption1:(currentSpace - 9),
                         moveOption2: null,
                     },
-                    57: { moveOption1: null, 
+                    56: { moveOption1: null, 
                         moveOption2: (currentSpace - 7), 
                     },
                 },
@@ -82,42 +106,42 @@ function moveCalculator(e) {
                 moveOption4: (currentSpace + 7),
                 restrictedSquares: {
                     
-                    8: { moveOption1: null, 
+                    7: { moveOption1: null, 
                         moveOption2: null, 
                         moveOption3: null, 
                         moveOption4: (currentSpace + 7), 
                     },
-                    9: { moveOption1: null, 
+                    8: { moveOption1: null, 
                         moveOption2: (currentSpace - 7), 
                         moveOption3: (currentSpace + 9), 
                         moveOption4: null, 
                     },
-                    24: { moveOption1:(currentSpace - 9),
+                    23: { moveOption1:(currentSpace - 9),
                         moveOption2: null,
                         moveOption3: null,
                         moveOption4: (currentSpace + 7),
                     },
-                    25: {moveOption1: null, 
+                    24: {moveOption1: null, 
                         moveOption2: (currentSpace - 7), 
                         moveOption3: (currentSpace + 9), 
                         moveOption4: null, 
                     },
-                    40: { moveOption1:(currentSpace - 9),
+                    39: { moveOption1:(currentSpace - 9),
                         moveOption2: null,
                         moveOption3: null,
                         moveOption4: (currentSpace + 7),
                     },
-                    41: {moveOption1: null, 
+                    40: {moveOption1: null, 
                         moveOption2: (currentSpace - 7), 
                         moveOption3: (currentSpace + 9), 
                         moveOption4: null,
                     },
-                    56: {  moveOption1:(currentSpace - 9),
+                    55: {  moveOption1:(currentSpace - 9),
                         moveOption2: null,
                         moveOption3: null,
                         moveOption4: (currentSpace + 7),
                     },
-                    57: {moveOption1: null, 
+                    56: {moveOption1: null, 
                         moveOption2: (currentSpace - 7), 
                         moveOption3: null, 
                         moveOption4: null, 
@@ -131,25 +155,25 @@ function moveCalculator(e) {
                 moveOption1: (currentSpace + 9),
                 moveOption2: (currentSpace + 7),
                 restrictedSquares: {
-                    9: { moveOption1: (currentSpace + 9), 
+                    8: { moveOption1: (currentSpace + 9), 
                         moveOption2: null, 
                     },
-                    24: { moveOption1: null,
+                    23: { moveOption1: null,
                         moveOption2: (currentSpace + 7),
                     },
-                    25: { moveOption1: (currentSpace + 9), 
+                    24: { moveOption1: (currentSpace + 9), 
                         moveOption2: null, 
                     },
-                    40: { moveOption1: null,
+                    39: { moveOption1: null,
                         moveOption2: (currentSpace + 7),
                     },
-                    41: { moveOption1: (currentSpace + 9), 
+                    40: { moveOption1: (currentSpace + 9), 
                         moveOption2: null, 
                     },
-                    56: { moveOption1: null,
+                    55: { moveOption1: null,
                         moveOption2: (currentSpace + 7),
                     },
-                    57: { moveOption1: (currentSpace + 9), 
+                    56: { moveOption1: (currentSpace + 9), 
                         moveOption2: null, 
                     },
                 },
@@ -162,42 +186,42 @@ function moveCalculator(e) {
                 moveOption4: (currentSpace + 7),
                 restrictedSquares: {
                     
-                    8: { moveOption1: null, 
+                    7: { moveOption1: null, 
                         moveOption2: (currentSpace + 7), 
                         moveOption3: null, 
                         moveOption4: null, 
                     },
-                    9: { moveOption1: (currentSpace + 9), 
+                    8: { moveOption1: (currentSpace + 9), 
                         moveOption2: null, 
                         moveOption3: null, 
                         moveOption4: (currentSpace - 7), 
                     },
-                    24: { moveOption1: null, 
+                    23: { moveOption1: null, 
                         moveOption2: (currentSpace + 7), 
                         moveOption3: (currentSpace - 9), 
                         moveOption4: null,
                     },
-                    25: { moveOption1: (currentSpace + 9), 
+                    24: { moveOption1: (currentSpace + 9), 
                         moveOption2: null, 
                         moveOption3: null, 
                         moveOption4: (currentSpace - 7),  
                     },
-                    40: { moveOption1: null, 
+                    39: { moveOption1: null, 
                         moveOption2: (currentSpace + 7), 
                         moveOption3: (currentSpace - 9), 
                         moveOption4:  null,
                     },
-                    41: { moveOption1: (currentSpace + 9), 
+                    40: { moveOption1: (currentSpace + 9), 
                         moveOption2: null, 
                         moveOption3: null, 
                         moveOption4: (currentSpace - 7), 
                     },
-                    56: { moveOption1: null, 
+                    55: { moveOption1: null, 
                         moveOption2: (currentSpace + 7), 
                         moveOption3: (currentSpace - 9), 
                         moveOption4: null,
                     },
-                    57: {moveOption1: null, 
+                    56: {moveOption1: null, 
                         moveOption2: null, 
                         moveOption3: null, 
                         moveOption4: (currentSpace - 7),  
@@ -208,17 +232,44 @@ function moveCalculator(e) {
     }
     
     //determine moveOptions
-    moveOption1 = moveLookup[tokenTeamSelected][tokenRankSelected][moveOption1];
-    moveOption2 = moveLookup[tokenTeamSelected][tokenRankSelected];
-    console.log(typeof currentSpace);
-    console.log(moveOption1);
-    console.log(moveOption2);
-    console.log(moveLookup[tokenTeamSelected][tokenRankSelected]);
+    moveOption1 = moveLookup[tokenTeamSelected][tokenRankSelected].moveOption1;
+    moveOption2 = moveLookup[tokenTeamSelected][tokenRankSelected].moveOption2;
+    console.log(`you are at ${currentSpace}`);
+    console.log(`you can move to ${moveOption1}`);
+    console.log(`you can move2 to ${moveOption2}`);
+    // console.log(moveLookup[tokenTeamSelected][tokenRankSelected]);
     
     //determine movement restrictions
-    // if (currentSpace = restrictedSquares[tokenTeamSelected][currentSpace]) {console.log(currentSpace)
-    // // };
-    // console.log(moveLookup[restrictedSquares][tokenTeamSelected][currentSpace]);
+    let restrictedCheck = currentSpace;
+    if (moveLookup[tokenTeamSelected][tokenRankSelected].restrictedSquares[restrictedCheck]) {
+        moveOption1 = moveLookup[tokenTeamSelected][tokenRankSelected].restrictedSquares[restrictedCheck].moveOption1;
+        moveOption2 = moveLookup[tokenTeamSelected][tokenRankSelected].restrictedSquares[restrictedCheck].moveOption2;
+        console.log(`updated move ${moveOption1}`);
+        console.log(`updated move2 ${moveOption2}`);
+    };
+
+    if (gameState[moveOption1] === playerTurn.tokenUsed) {
+        moveOption1 = null;
+    };
+    if (gameState[moveOption2] === playerTurn.tokenUsed) {
+        moveOption2 = null;
+    };
+    
+    //display move options on board
+    if (moveOption1 !== null) {
+    let moveOption1Display = moveOption1.toString();
+    document.getElementById(moveOption1Display).classList.add("potentialMove");
+    };
+    if (moveOption2 !== null) {
+    let moveOption2Display = moveOption2.toString();
+    document.getElementById(moveOption2Display).classList.add("potentialMove");
+    };
+
+   
+
+
+
+
 }
 
 function tokenTypeLookup (e) { 
@@ -243,7 +294,9 @@ function turnCheck (e) {
     };
 
 
-
+function moveToken (e) {
+    console.log(e.target);
+};
 
 
 
