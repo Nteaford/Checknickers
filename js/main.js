5adf42825b71479ebb3fa9d052a656ad1f5aa96d
Nick Teaford <nickteaford@Nicks-iMac.local>
2021-11-30 17:33:19 -0800

    Tuesday Nov 30th evening commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 /*----- app's state (variables) -----*/
-let playerTurn, currentSelection, currentSpace, checkSpace, newSpace, jumpCheckSpace, enemySquare1, enemySquare2, enemySquare3, enemySquare4, moveOption1, moveOption2, moveOption3, moveOption4, jumpOption1, jumpOption2, jumpOption3, jumpOption4, tokenTeamSelected, tokenRankSelected, tokenId, jumpedCheck;
-
+let playerTurn, currentSpace, checkSpace, newSpace, jumpCheckSpace, enemySquare1, enemySquare2, enemySquare3, enemySquare4, moveOption1, moveOption2, moveOption3, moveOption4, jumpOption1, jumpOption2, jumpOption3, jumpOption4, tokenTeamSelected, tokenRankSelected, tokenId;
 
 /*----- constants -----*/
 let gameState = [];
@@ -14,24 +13,19 @@ const redTurnLookup = {
     opponentToken: "black--token",
 }
 
-const restrictedSquareAlert = [7, 8, 23, 24, 39, 40, 55, 56]
-
 const upgradeSquares = {
     "black--token": [1, 3, 5, 7],
     "red--token": [56, 58, 60, 62],
 }
 
-
 /*----- cached element references -----*/
 let blackTurnTextEl = document.querySelector(".black-turn-text");
 let redTurnTextEl = document.querySelector(".red-turn-text");
 
+let winTextEl = document.getElementById("win-text");
 
 /*----- event listeners -----*/
 document.querySelector(".game--container").addEventListener('click', moveCalculator);
-
-
-
 
 /*----- functions -----*/
 function init() {
@@ -39,15 +33,8 @@ function init() {
     redTurnTextEl.style.color = "grey";
     blackTurnTextEl.style.color = "black";
     currentSpace = 0;
-
     gameState = ["", "red--token", "", "red--token", "", "red--token", "", "red--token", "red--token", "", "red--token", "", "red--token", "", "red--token", "", "", "red--token", "", "red--token", "", "red--token", "", "red--token", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "black--token", "", "black--token", "", "black--token", "", "black--token", "", "", "black--token", "", "black--token", "", "black--token", "", "black--token", "black--token", "", "black--token", "", "black--token", "", "black--token", ""];
-
-    render();
 };
-function render() {
-}
-
-
 
 init();
 
@@ -55,18 +42,22 @@ function moveCalculator(e) {
     //reset checkerboard squares to grey
     let yellowClear = document.querySelectorAll(".potentialMove");
     yellowClear.forEach(checkerbox => checkerbox.classList.remove("potentialMove"));
+
     //set current token type
     tokenRankSelected = tokenTypeLookup(e);
+
     //confirm that this token is allowed to move
     tokenTeamSelected = tokenTeamLookup(e);
+
     //set currentSpace variable
     currentSpace = parseInt(currentSpace = e.target.parentElement.getAttribute("id"));
+
     //set checkSpace variable;
     checkSpace = currentSpace;
 
     //collect tokenId
     tokenId = e.target.getAttribute(("id"));
-    // console.log(tokenId);
+
 
     //create moveLookup Object
     let moveLookup = moveLookupFunction(checkSpace);
@@ -78,86 +69,24 @@ function moveCalculator(e) {
 
     //determine moveOptions
     determineMoveOptions();
-    // console.log(`MC: you are at ${checkSpace}`);
+    
+    //determine movement restrictions
+    determineMoveRestrictions();
+    
+    //Determine Team Block
+    determineTeamBlock();
+    
+    //Jump Checking
+    jumpCheck();
+    
+    
     function determineMoveOptions() {
         moveOption1 = moveLookup[tokenTeamSelected][tokenRankSelected].moveOption1;
         moveOption2 = moveLookup[tokenTeamSelected][tokenRankSelected].moveOption2;
         moveOption3 = moveLookup[tokenTeamSelected][tokenRankSelected].moveOption3;
         moveOption4 = moveLookup[tokenTeamSelected][tokenRankSelected].moveOption4;
-    }
-    
-    
-    //determine movement restrictions
-    determineMoveRestrictions();
-    //Jump Checking
-    jumpCheck();
-    
-
-
-    function jumpCheck() {
-        if (gameState[moveOption1] === playerTurn.opponentToken) {
-            let jumpLookup = jumpLookupFunction(checkSpace);
-            jumpOption1 = jumpLookup[tokenTeamSelected][tokenRankSelected].jumpOption1;
-
-            if ((gameState[jumpOption1] !== "")) {
-                moveOption1 = null;
-            } else {
-                enemySquare1 = moveOption1;
-                determineJumpRestrictions();
-                moveOption1 = jumpOption1;
-            }
-        }
-        if (gameState[moveOption2] === playerTurn.opponentToken) {
-            let jumpLookup = jumpLookupFunction(checkSpace);
-            jumpOption2 = jumpLookup[tokenTeamSelected][tokenRankSelected].jumpOption2;
-            
-            if ((gameState[jumpOption2] !== "")) {
-                moveOption2 = null;
-            } else {
-                enemySquare2 = moveOption2;
-                determineJumpRestrictions();
-                moveOption2 = jumpOption2;
-            }
-        }
-        if (gameState[moveOption3] === playerTurn.opponentToken) {
-            let jumpLookup = jumpLookupFunction(checkSpace);
-            jumpOption3 = jumpLookup[tokenTeamSelected][tokenRankSelected].jumpOption3;
-            
-            if ((gameState[jumpOption3] !== "")) {
-                moveOption3 = null;
-            } else {
-                enemySquare3 = moveOption3;
-                determineJumpRestrictions();
-                moveOption3 = jumpOption3;
-            }
-        }
-        if (gameState[moveOption4] === playerTurn.opponentToken) {
-            let jumpLookup = jumpLookupFunction(checkSpace);
-            jumpOption4 = jumpLookup[tokenTeamSelected][tokenRankSelected].jumpOption4;
-            
-            if ((gameState[jumpOption4] !== "")) {
-                moveOption4 = null;
-            } else {
-                enemySquare4 = moveOption4;
-                determineJumpRestrictions();
-                moveOption4 = jumpOption4;
-            }
-        }
     };
 
-
-    function determineJumpRestrictions() {
-        let jumpLookup = jumpLookupFunction(checkSpace);
-        let restrictedJumpCheck = currentSpace;
-        if (jumpLookup[tokenTeamSelected][tokenRankSelected].restrictedSquares[restrictedJumpCheck]) {
-            jumpOption1 = jumpLookup[tokenTeamSelected][tokenRankSelected].restrictedSquares[restrictedJumpCheck].jumpOption1;
-            jumpOption2 = jumpLookup[tokenTeamSelected][tokenRankSelected].restrictedSquares[restrictedJumpCheck].jumpOption2;
-            jumpOption3 = jumpLookup[tokenTeamSelected][tokenRankSelected].restrictedSquares[restrictedJumpCheck].jumpOption3;
-            jumpOption4 = jumpLookup[tokenTeamSelected][tokenRankSelected].restrictedSquares[restrictedJumpCheck].jumpOption4;
-
-        }
-    };
-    
     function determineMoveRestrictions() {
         let restrictedCheck = currentSpace;
         if (moveLookup[tokenTeamSelected][tokenRankSelected].restrictedSquares[restrictedCheck]) {
@@ -165,16 +94,10 @@ function moveCalculator(e) {
             moveOption2 = moveLookup[tokenTeamSelected][tokenRankSelected].restrictedSquares[restrictedCheck].moveOption2;
             moveOption3 = moveLookup[tokenTeamSelected][tokenRankSelected].restrictedSquares[restrictedCheck].moveOption3;
             moveOption4 = moveLookup[tokenTeamSelected][tokenRankSelected].restrictedSquares[restrictedCheck].moveOption4;
-        }
-        // console.log(`MC: updated move ${moveOption1}`);
-        // console.log(`MC: updated move2 ${moveOption2}`);
-        // console.log(`MC: updated move3 ${moveOption3}`);
-        // console.log(`MC: updated move4 ${moveOption4}`);
+        };
     };
 
 
-    determineTeamBlock();
-    //Team Block Check
     function determineTeamBlock() {
         if (gameState[moveOption1] === playerTurn.tokenUsed) {
             moveOption1 = null;
@@ -183,10 +106,10 @@ function moveCalculator(e) {
             moveOption2 = null;
         };
         if (gameState[moveOption3] === playerTurn.tokenUsed) {
-            moveOption1 = null;
+            moveOption3 = null;
         };
         if (gameState[moveOption4] === playerTurn.tokenUsed) {
-            moveOption2 = null;
+            moveOption4 = null;
         };
 
         //Board Block Check
@@ -204,9 +127,71 @@ function moveCalculator(e) {
         };
     };
 
+    function jumpCheck() {
+        if (gameState[moveOption1] === playerTurn.opponentToken) {
+            let jumpLookup = jumpLookupFunction(checkSpace);
+            jumpOption1 = jumpLookup[tokenTeamSelected][tokenRankSelected].jumpOption1;
 
-    displayMoveOptions();
-    //display move options on board
+            if ((gameState[jumpOption1] !== "")) {
+                moveOption1 = null;
+            } else {
+                enemySquare1 = moveOption1;
+                determineJumpRestrictions();
+                moveOption1 = jumpOption1;
+
+            }
+        };
+
+        if (gameState[moveOption2] === playerTurn.opponentToken) {
+            let jumpLookup = jumpLookupFunction(checkSpace);
+            jumpOption2 = jumpLookup[tokenTeamSelected][tokenRankSelected].jumpOption2;
+
+            if ((gameState[jumpOption2] !== "")) {
+                moveOption2 = null;
+            } else {
+                enemySquare2 = moveOption2;
+                determineJumpRestrictions();
+                moveOption2 = jumpOption2;
+            };
+        };
+
+        if (gameState[moveOption3] === playerTurn.opponentToken) {
+            let jumpLookup = jumpLookupFunction(checkSpace);
+            jumpOption3 = jumpLookup[tokenTeamSelected][tokenRankSelected].jumpOption3;
+
+            if ((gameState[jumpOption3] !== "")) {
+                moveOption3 = null;
+            } else {
+                enemySquare3 = moveOption3;
+                determineJumpRestrictions();
+                moveOption3 = jumpOption3;
+            }
+        };
+        if (gameState[moveOption4] === playerTurn.opponentToken) {
+            let jumpLookup = jumpLookupFunction(checkSpace);
+            jumpOption4 = jumpLookup[tokenTeamSelected][tokenRankSelected].jumpOption4;
+
+            if ((gameState[jumpOption4] !== "")) {
+                moveOption4 = null;
+            } else {
+                enemySquare4 = moveOption4;
+                determineJumpRestrictions();
+                moveOption4 = jumpOption4;
+            }
+        };
+    };
+
+    function determineJumpRestrictions() {
+        let jumpLookup = jumpLookupFunction(checkSpace);
+        let restrictedJumpCheck = currentSpace;
+        if (jumpLookup[tokenTeamSelected][tokenRankSelected].restrictedSquares[restrictedJumpCheck]) {
+            jumpOption1 = jumpLookup[tokenTeamSelected][tokenRankSelected].restrictedSquares[restrictedJumpCheck].jumpOption1;
+            jumpOption2 = jumpLookup[tokenTeamSelected][tokenRankSelected].restrictedSquares[restrictedJumpCheck].jumpOption2;
+            jumpOption3 = jumpLookup[tokenTeamSelected][tokenRankSelected].restrictedSquares[restrictedJumpCheck].jumpOption3;
+            jumpOption4 = jumpLookup[tokenTeamSelected][tokenRankSelected].restrictedSquares[restrictedJumpCheck].jumpOption4;
+        }
+    };
+
     function displayMoveOptions() {
         if (moveOption1 !== null) {
             let moveOption1Display = moveOption1.toString();
@@ -226,15 +211,14 @@ function moveCalculator(e) {
         };
     };
 
-    // jumpCheck();
-    // displayMoveOptions();
+    //display move options on board
+    displayMoveOptions();
 
+    //Add event listeners to the yellow display squares
     const moveSetup = document.querySelectorAll(".potentialMove");
     for (let i = 0; i < moveSetup.length; i++) {
         moveSetup[i].addEventListener("click", moveToken)
     };
-
-
 };
 
 function tokenTypeLookup(e) {
@@ -249,9 +233,7 @@ function tokenTeamLookup(e) {
     tokenTeamHalfway = tokenTeamHalfway.split(" ");
     return tokenTeam = tokenTeamHalfway[0];
     // console.log(tokenRank);
-
 }
-
 
 function moveLookupFunction(checkSpace) {
     const moveLookup = {
@@ -312,7 +294,6 @@ function moveLookupFunction(checkSpace) {
                 moveOption3: (checkSpace + 9),
                 moveOption4: (checkSpace + 7),
                 restrictedSquares: {
-
                     7: {
                         moveOption1: null,
                         moveOption2: null,
@@ -422,14 +403,12 @@ function moveLookupFunction(checkSpace) {
                     },
                 },
             },
-
             king: {
-                moveOption1: (checkSpace - 9),
-                moveOption2: (checkSpace - 7),
-                moveOption3: (checkSpace + 9),
-                moveOption4: (checkSpace + 7),
+                moveOption1: (checkSpace + 9),
+                moveOption2: (checkSpace + 7),
+                moveOption3: (checkSpace - 9),
+                moveOption4: (checkSpace - 7),
                 restrictedSquares: {
-
                     7: {
                         moveOption1: null,
                         moveOption2: (checkSpace + 7),
@@ -591,7 +570,6 @@ function jumpLookupFunction(checkSpace) {
                         jumpOption3: null,
                         jumpOption4: null,
                     },
-
                 },
             },
             king: {
@@ -600,7 +578,6 @@ function jumpLookupFunction(checkSpace) {
                 jumpOption3: (checkSpace + 18),
                 jumpOption4: (checkSpace + 14),
                 restrictedSquares: {
-
                     1: {
                         jumpOption1: null,
                         jumpOption2: null,
@@ -750,7 +727,6 @@ function jumpLookupFunction(checkSpace) {
             },
         },
         "red--token": {
-
             soldier: {
                 jumpOption1: (checkSpace + 18),
                 jumpOption2: (checkSpace + 14),
@@ -808,8 +784,8 @@ function jumpLookupFunction(checkSpace) {
                         jumpOption4: null,
                     },
                     24: {
-                        jumpOption1: null,
-                        jumpOption2: (checkSpace + 14),
+                        jumpOption1: (checkSpace + 18),
+                        jumpOption2: null,
                         jumpOption3: null,
                         jumpOption4: null,
                     },
@@ -887,7 +863,6 @@ function jumpLookupFunction(checkSpace) {
                 jumpOption3: (checkSpace - 18),
                 jumpOption4: (checkSpace - 14),
                 restrictedSquares: {
-
                     1: {
                         jumpOption1: (checkSpace + 18),
                         jumpOption2: null,
@@ -1040,27 +1015,20 @@ function jumpLookupFunction(checkSpace) {
     return jumpLookup;
 }
 
-
 function moveToken(e) {
     if (e.target.tagName === "P") return;
     if (tokenTeam !== playerTurn.tokenUsed)
         return;
 
-    let movingToken = currentSpace;
-    console.log(e.target.id);
     //removal of old moving piece
     let oldSpaceDom = document.getElementById(tokenId);
-
-    console.log(oldSpaceDom);
     oldSpaceDom.remove(document.querySelector(tokenId));
     gameState[currentSpace] = "";
-
 
     //addition of new moving piece
     let newSpaceDom = document.createElement("p");
     newSpace = parseInt(e.target.id);
     gameState[newSpace] = playerTurn.tokenUsed;
-
     if (upgradeSquares[playerTurn.tokenUsed].some(x => x === newSpace)) {
         newSpaceDom.classList.add(tokenTeamSelected, "king");
         newSpaceDom.setAttribute("id", tokenId);
@@ -1068,69 +1036,61 @@ function moveToken(e) {
         newSpaceDom.classList.add(tokenTeamSelected, tokenRankSelected);
         newSpaceDom.setAttribute("id", tokenId);
     };
-
-
-
     let newSpaceDomLocator = e.target;
     newSpaceDomLocator.appendChild(newSpaceDom);
-
-
-    console.log(newSpace);
-    console.log(jumpCheckSpace);
-    console.log(enemySquare1);
 
     //Removal of jumped piece
     if (newSpace === jumpOption1) {
         let enemySpaceDom = document.getElementById(enemySquare1);
         enemySpaceDom.removeChild(enemySpaceDom.lastChild);
         gameState[enemySquare1] = "";
-        jumpedCheck = true;
-
     }
-    else if (newSpace === jumpOption2) {
+     if (newSpace === jumpOption2) {
         let enemySpaceDom = document.getElementById(enemySquare2);
         enemySpaceDom.removeChild(enemySpaceDom.lastChild);
         gameState[enemySquare2] = "";
-        jumpedCheck = true;
     }
-    else if (newSpace === jumpOption3) {
+     if (newSpace === jumpOption3) {
         let enemySpaceDom = document.getElementById(enemySquare3);
         enemySpaceDom.removeChild(enemySpaceDom.lastChild);
         gameState[enemySquare3] = "";
-        jumpedCheck = true;
     }
-    else if (newSpace === jumpOption4) {
+     if (newSpace === jumpOption4) {
         let enemySpaceDom = document.getElementById(enemySquare4);
         enemySpaceDom.removeChild(enemySpaceDom.lastChild);
         gameState[enemySquare4] = "";
-        jumpedCheck = true;
-    } else jumpedCheck = false;
-
-    //
-    if (jumpedCheck = true) {
-        currentSpace = newSpace;
-        checkSpace = currentSpace;
-        moveLookup = moveLookupFunction(checkSpace);
-
-    };
-
-    function doubleJump() {
-
-    }
-
+    }  
 
     //turn switch
     if (playerTurn === blkTurnLookup) {
         playerTurn = redTurnLookup;
         redTurnTextEl.style.color = "red";
-        blackTurnTextEl.style.color = "grey";
+        blackTurnTextEl.style.color = "lightgrey";
     } else {
         playerTurn = blkTurnLookup;
-        redTurnTextEl.style.color = "grey";
+        redTurnTextEl.style.color = "lightgrey";
         blackTurnTextEl.style.color = "black";
     };
+
     //reset variables
     tokenTrackingReset();
+
+    function tokenTrackingReset() {
+        tokenRankSelected = "";
+        tokenTeamSelected = "";
+        currentSpace = "";
+        enemySquare1 - "";
+        enemySquare2 - "";
+        enemySquare3 - "";
+        enemySquare4 - "";
+        checkSpace = currentSpace;
+        tokenId = "";
+        moveOption1 = "";
+        moveOption2 = "";
+        moveOption3 = "";
+        moveOption4 = "";
+    
+    }
 
     //remove event listeners
     const moveEventRemove = document.querySelectorAll(".potentialMove");
@@ -1138,23 +1098,9 @@ function moveToken(e) {
         moveEventRemove[i].removeEventListener("click", moveToken)
     };
 
-
+    //win condition text
+    if (!gameState.some(x => x === "black--token"))  winTextEl.innerText = "Red Wins!";
+    winTextEl.style.color = "red"
+    if (!gameState.some(x => x === "red--token")) winTextEl.innerText = "Black Wins!";
+    winTextEl.style.color = "black"
 };
-
-
-function tokenTrackingReset() {
-    tokenRankSelected = "";
-    tokenTeamSelected = "";
-    currentSpace = "";
-    enemySquare1 - "";
-    enemySquare2 - "";
-    enemySquare3 - "";
-    enemySquare4 - "";
-    checkSpace = currentSpace;
-    tokenId = "";
-    moveOption1 = "";
-    moveOption2 = "";
-    moveOption3 = "";
-    moveOption4 = "";
-
-}
